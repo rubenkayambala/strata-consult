99bb4ec056e6ea9ed565acb5b65a21f462cf49b3
rubenkayambala <rubenkayambala@gmail.com>
2023-11-30 19:05:30 +0100

    Images in home page

--- strata/settings.py
+++ strata/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+STATICFILES_DIRS = [ BASE_DIR / 'statics' ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
